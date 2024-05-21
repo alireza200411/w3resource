4e375fb3a9eb4e2641fd3571f857d78e45f6a6fb
alireza200411 <alirezamoradi292048@gmail.com>
2024-05-21 17:45:07 +0330

    Solving a question from the topic of dictionary

--- dice.py
+++ dice.py
@@ -14,9 +14,28 @@
 
 # -------------------------------------
 # peactice 23
-mainlist = []
-counter = int(input('enter the counter: '))
-for i in range(1 , counter + 1) : 
-    sublist = []
-    indx1 = input('enter the first element: ')
-    indx2 = input('enter the second element: ')
+def frequency(counter: int) -> dict : 
+    dic = {}
+    mainlist = []
+    # counter = int(input('enter the counter: '))
+    for i in range(1 , counter + 1) : 
+        print(f'Getting the {i} list data')
+        sublist = []
+        indx1 = input('enter the first element: ')
+        indx2 = input('enter the second element: ')
+        sublist.append(indx1)
+        sublist.append(indx2)
+        mainlist.append(sublist)
+    orderlist = []
+    orderlist.append(tuple(mainlist))
+    # print(orderlist)
+    for data in orderlist : 
+        # print(type(data))
+        for d in data : 
+            # print(type(d))
+            x = data.count(d)
+            s = str(d)
+            dic[s] = x
+
+    return dic       
+
